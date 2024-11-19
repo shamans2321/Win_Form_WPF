88432d2b9d229ad46f7084a734dead37c0400c61
Никита Паутов <145455384+shamans2321@users.noreply.github.com>
2024-11-19 10:52:41 +0300

    prak_rab_13

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+            foreach (DriveInfo drive in drives)
+            {
+                Console.WriteLine($"Название:{drive.Name}");
+                Console.WriteLine($"Тип: {drive.DriveType}");
+                if (drive.IsReady)
+                {
+                    Console.WriteLine($"Файловая система:{drive.DriveFormat }");
+                    Console.WriteLine($"Объём диска:{drive.TotalSize }");
+                    Console.WriteLine($"Свободное пространство:{drive.TotalFreeSpace }");
+                    Console.WriteLine($"Метка:{drive.VolumeLabel }");
+                    Console.WriteLine($"Коневой каталог:{drive.RootDirectory }");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
